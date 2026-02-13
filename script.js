c2e38f948c6ec1e48cdaff44852ccacb82ae215c
devmarquinhos <marcos.emanuel.04@gmail.com>
2026-02-13 10:43:21 -0300

    :sparkles: add audio loop with one of her fav songs, heart confetti for yes btn

--- script.js
+++ script.js
@@ -9,6 +9,23 @@ const catImg = document.getElementById("letter-cat");
 const buttons = document.getElementById("letter-buttons");
 const finalText = document.getElementById("final-text");
 
+const audio = document.getElementById("bg-music");
+
+// Frases caso ela tente clicar não
+const phrases = [
+    "Tem certeza disso?",
+    "Pensa com carinho...",
+    "Oh mor, não faz isso comigo!",
+    "Vou ficar tão titi...",
+    "O outro botão é mais bonito!",
+    "Olha para mim, o gatinho preto...",
+    "Sério mesmo?",
+    "Faço qualquer coisa!",
+    "Essa música me lembra a gente...",
+    "Quero casar com você!",
+    "Prometo ser o melhor gato preto da sua vida!"
+];
+
 // Click Envelope
 envelope.addEventListener("click", () => {
     envelope.style.display = "none";
@@ -17,12 +34,15 @@ envelope.addEventListener("click", () => {
     setTimeout( () => {
         document.querySelector(".letter-window").classList.add("open");
     },50);
+
+    audio.volume = 0.095;
+    audio.play();
 });
 
 // Mover o botão pela pág
 noBtn.addEventListener("mouseover", () => {
-    const min = 600;
-    const max = 600;
+    const min = 200;
+    const max = 400;
 
     const distance = Math.random() * (max - min) + min;
     const angle = Math.random() * Math.PI * 4;
@@ -32,17 +52,46 @@ noBtn.addEventListener("mouseover", () => {
 
     noBtn.style.transition = "transform 0.3s ease";
     noBtn.style.transform = `translate(${moveX}px, ${moveY}px)`;
+
+    const randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
+    title.textContent = randomPhrase;
 });
 
 // Clicar sim
 yesBtn.addEventListener("click", () => {
     title.textContent = "Seu gatinho preto ficou mais feliz!";
-
     catImg.src = "assets/cat_dance.gif";
 
     document.querySelector(".letter-window").classList.add("final");
-
     buttons.style.display = "none";
-
     finalText.style.display = "block";
+
+    createHeartShower();
 });
+
+// Chuva de corações
+function createHeartShower() {
+    const heartCount = 50;
+
+    for (let i = 0; i < heartCount; i++) {
+        setTimeout(() => {
+            const heart = document.createElement("div");
+            heart.classList.add("heart-confetti");
+            heart.innerHTML = "❤️"; 
+
+            heart.style.left = Math.random() * 100 + "vw";
+
+            const size = Math.random() * 20 + 20; 
+            heart.style.fontSize = `${size}px`;
+
+            const duration = Math.random() * 3 + 2;
+            heart.style.animationDuration = `${duration}s`;
+            
+            document.body.appendChild(heart);
+
+            setTimeout(() => {
+                heart.remove();
+            }, duration * 1000);
+        }, i * 100);
+    }
+}
